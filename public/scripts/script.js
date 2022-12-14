931c82f4b610396eea4052dcb7abf144913329ee
unknown <coolshiv0721@gmail.com>
2022-12-14 16:21:41 +0530

    Navbar added

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,14 +1,14 @@
 // Open links in mobiles
 function handleSmallScreens() {
-  document.querySelector(".navbar-toggler").addEventListener("click", () => {
-    let navbarMenu = document.querySelector(".navbar-menu");
+  document.querySelector('.navbar-toggler').addEventListener('click', () => {
+    let navbarMenu = document.querySelector('.navbar-menu');
 
-    if (navbarMenu.style.display === "flex") {
-      navbarMenu.style.display = "none";
+    if (navbarMenu.style.display === 'flex') {
+      navbarMenu.style.display = 'none';
       return;
     }
 
-    navbarMenu.style.display = "flex";
+    navbarMenu.style.display = 'flex';
   });
 }
 
