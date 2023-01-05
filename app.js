97a34d5ef7e2bea7e298e21d5821ff32543a1c1d
saurabh bakolia <skbakolia2002@gmail.com>
2023-01-05 18:44:04 +0530

    pug engine setup

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ require('dotenv').config()
 /* ---------------------------- Basic Connections --------------------------- */
 const app = express();
 
+/* ----------------------------- Set View Engine ---------------------------- */
+app.set('view engine', 'pug');
+
+/* ---------------------------- Set Views Folder ---------------------------- */
+app.set('views', path.join(__dirname, 'src', 'views'));
 
 /* ---------------------------- Set Static Folder --------------------------- */
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 /* ---------------------------- Setting up CSS/JS Files --------------------------- */
@@ -28,11 +34,11 @@ app.use(bodyParser.urlencoded({
 
 /* ---------------------------- Basic Routes --------------------------- */
 app.get("/", function (req, res,) {
-  res.sendFile(__dirname + "/index.html");
+  res.render('index');
 });
 
 app.get("/about", function (req, res,) {
-  res.sendFile(__dirname + "/about.html");
+  res.render('about');
 });
 
 
@@ -42,20 +48,15 @@ app.post("/", function (req, res) {
   const unit = "metric"
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
   https.get(url, function (response) {
-    // console.log(response);
     console.log(response.statusCode);
 
     response.on("data", function (data) {
       const weatherData = JSON.parse(data)
-      // console.log(weatherData);
 
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
       const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-
-
-
       res.write("<p>The weather is currently " + weatherDescription + " </p>");
       res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
       res.write("<img src=" + imageURL + ">");
